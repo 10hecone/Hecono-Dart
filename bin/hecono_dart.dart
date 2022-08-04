160d88553f42ccbf8e62aaa9de04cc1ef7533c74
Amir Anglo <angloamir@gmail.com>
2022-08-04 16:22:48 +0200

    Add command debug content, Test event onGuildMemberAdd #3

--- bin/hecono_dart.dart
+++ bin/hecono_dart.dart
@@ -7,19 +7,42 @@ void main() {
   final bot = NyxxFactory.createNyxxWebsocket(
       '$envToken',
       GatewayIntents.guilds |
-      GatewayIntents.messageContent |
-      GatewayIntents.guildMessages
-    );
-
+          GatewayIntents.messageContent |
+          GatewayIntents.guildMessages);
 
   bot.eventsWs.onMessageReceived.listen((event) {
-    final replyBuilder = ReplyBuilder.fromMessage(event.message);
-    final allowedMentions = AllowedMentions();
+    if (event.message.author.bot) return;
 
-    if (event.message.content == "!ping") {
-      event.message.channel.sendMessage(
-          MessageBuilder.content('Pong!')..replyBuilder = replyBuilder..allowedMentions = allowedMentions);
+    String prefix = '!';
+
+    final replyBuilder = ReplyBuilder.fromMessage(event.message);
+    final embedBuiderContent = EmbedBuilder()
+      ..title = 'Command: ${event.message.content.substring(1)}'
+      ..description = '''
+      Content: ${event.message.content}
+      Length: ${event.message.content.length}
+      Author: ${event.message.author}
+      '''
+      ..color = DiscordColor.fromRgb(140, 140, 140);
+    final embedBuilderPing = EmbedBuilder()
+      ..title = 'Command: ${event.message.content.substring(1)}'
+      ..description = 'Pong!'
+      ..color = DiscordColor.fromRgb(140, 140, 140);
+
+    if (event.message.content == "${prefix}ping") {
+      event.message.channel.sendMessage(MessageBuilder.content(' ')
+        ..replyBuilder = replyBuilder
+        ..embeds = [embedBuilderPing]);
+    } else if (event.message.content ==
+        "$prefix${event.message.content.substring(1)}") {
+      event.message.channel.sendMessage(MessageBuilder.content(' ')
+        ..replyBuilder = replyBuilder
+        ..embeds = [embedBuiderContent]);
     }
+  });
+
+  bot.eventsWs.onGuildMemberAdd.listen((event) {
+    print(event.member.id);
   });
 
   bot.connect();
